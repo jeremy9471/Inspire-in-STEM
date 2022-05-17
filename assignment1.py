e580e0a20394e665384cce55568d471517d0e8da
jeremy9471 <hefty9471@gmail.com>
2022-05-17 10:31:20 +0000

    assignment1.py about arithmetic operations

--- assignment1.py
+++ assignment1.py
@@ -1,19 +1,19 @@
 # calculate area of a circle, volume and surface area of a cylinder and volume of a cube ( get user input)
 
-# area
+# area of cylinder
 radius = input("Enter radius of the circle ")
 pi = 3.142
 area = pi * int(radius) * int(radius)
 print("The area of the circle is " + str(area))
 
-# volume
+# volume of cylinder
 height = input("Enter height of the cylinder ")
-volume = area * int(height)
+baseRadius = input("Enter base radius of cylinder ")
+volume = pi * int(baseRadius) * int(baseRadius) * int(height)
 print("The volume of the cylinder is " + str(volume))
 
 # surface area
-circumfrence = int(radius) * 2 *pi
-surfaceArea = int(circumfrence) * int(height)
+surfaceArea = int(baseRadius) * 2 *pi * int(height)
 print("The surface area of the cylinder is " + str(surfaceArea))
 
 
@@ -21,3 +21,9 @@ print("The surface area of the cylinder is " + str(surfaceArea))
 length = input("Enter length of cube ")
 volume = int(length) * int(length) * int(length)
 print("Volume of the cube is " + str(volume))
+
+#  surface area of a cube
+surfaceAreaCube = int(length) * int(length) * 6
+print("The surface area of the cube is " + str(surfaceAreaCube))
+
+
