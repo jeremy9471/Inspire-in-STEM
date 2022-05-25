562e48a5021949e449a5f3e5f1e3694916d7a3f0
jeremy9471 <hefty9471@gmail.com>
2022-05-25 16:45:17 +0000

    assignment6..py on while and for loops with lists

--- assignment6..py
+++ assignment6..py
@@ -1,1 +1,7 @@
 # assignment print colors in uppercase once using a loop
+colors = ['red','blue','green','yellow']
+i = 0
+while i < len(colors):
+    for color in colors:
+        print(color.upper())
+    i = i+1
