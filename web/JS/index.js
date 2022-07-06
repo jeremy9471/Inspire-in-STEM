c95345f3adc3aae15eb1148f1480f39d52fa28af
jeremy9471 <105627571+jeremy9471@users.noreply.github.com>
2022-07-06 10:57:45 +0300

    web development

---
+++ web/JS/index.js
@@ -0,0 +1,12 @@
+/*multi line
+comment*/
+// single line comment in Java script
+
+let num1 = 17
+let num2 = 16
+sum_n = num1 + num2
+console.log(sum_n)
+
+function submitButton(){
+    alert("Button Clicked!")
+}
